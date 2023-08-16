d149830cf81921546f6f035dfa9dd4d0f080da45
lineno2 <hosun1804@gmail.com>
2023-08-16 12:46:52 +0900

    [REFACTOR] add new model for dio interceptor and update onResponse function #31

---
+++ lib/common/models/default_transfer_model.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'default_transfer_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+DefualtTransferModel _$DefualtTransferModelFromJson(
+        Map<String, dynamic> json) =>
+    DefualtTransferModel(
+      code: json['code'] as int,
+      msg: json['msg'] as String,
+      data: json['data'],
+    );
+
+Map<String, dynamic> _$DefualtTransferModelToJson(
+        DefualtTransferModel instance) =>
+    <String, dynamic>{
+      'code': instance.code,
+      'msg': instance.msg,
+      'data': instance.data,
+    };
