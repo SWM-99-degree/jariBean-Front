191e503e94800ddc7b475545c510c9eb5f4efb4c
lineno2 <hosun1804@gmail.com>
2023-08-16 13:38:39 +0900

    [REFACTOR] add new model for button, add factory constructor to load button from mode #35

--- lib/common/component/custom_button.dart
+++ lib/common/component/custom_button.dart
@@ -1,34 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jari_bean/common/const/color.dart';
+import 'package:jari_bean/common/models/custom_button_model.dart';
 
 class CustomButton extends StatelessWidget {
-  final double? width;
   final String text;
   final bool isDisabled;
-  final Function() onPressed;
-  const CustomButton(
-      {this.width,
-      required this.text,
-      required this.onPressed,
-      this.isDisabled = false,
-      Key? key})
-      : super(key: key);
+  final bool isDismiss;
+  final Function()? onPressed;
+  const CustomButton({
+    required this.text,
+    this.onPressed,
+    this.isDisabled = false,
+    this.isDismiss = false,
+    Key? key,
+  }) : super(key: key);
+
+  factory CustomButton.fromModel({
+    required CustomButtonModel model,
+  }) {
+    return CustomButton(
+      text: model.title,
+      onPressed: model.onPressed,
+      isDisabled: model.isDisabled ?? false,
+      isDismiss: model.isDismiss ?? false,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: isDisabled ? null : onPressed,
+      onPressed: isDisabled
+          ? null
+          : onPressed ??
+              () {
+                Navigator.pop(context);
+              },
       style: ElevatedButton.styleFrom(
-        backgroundColor: isDisabled == true ? GRAY_2 : PRIMARY_YELLOW,
+        backgroundColor: isDisabled == true
+            ? GRAY_2
+            : isDismiss
+                ? GRAY_2
+                : PRIMARY_YELLOW,
         shadowColor: Colors.transparent,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
-        fixedSize: const Size(335, 52),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(6.r),
+        ),
+        fixedSize: Size(
+          335.w,
+          56.h,
+        ),
       ),
       child: Text(
         text,
-        style: const TextStyle(
+        style: TextStyle(
           color: TEXTFIELD_INNER,
-          fontSize: 16,
+          fontSize: 16.sp,
           fontWeight: FontWeight.w700,
         ),
       ),
