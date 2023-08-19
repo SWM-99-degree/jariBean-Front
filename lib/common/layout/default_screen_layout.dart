7edb3e5096c19ad055b1dc62f19b09144b1f9531
lineno2 <hosun1804@gmail.com>
2023-08-19 21:51:59 +0900

    [REFACTOR] add widget parameter to default layout screen's appbar #50

--- lib/common/layout/default_screen_layout.dart
+++ lib/common/layout/default_screen_layout.dart
@@ -1,39 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DefaultLayout extends StatelessWidget {
   final Widget child;
   final Color? backgroundColor;
   final String? title;
+  final Widget? titleWidget;
   final Widget? bottomNavigationBar;
 
-  const DefaultLayout(
-      {required this.child,
-      this.backgroundColor,
-      this.title,
-      this.bottomNavigationBar,
-      Key? key})
-      : super(key: key);
+  const DefaultLayout({
+    required this.child,
+    this.backgroundColor,
+    this.title,
+    this.titleWidget,
+    this.bottomNavigationBar,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: renderAppBar(),
+      appBar: renderAppBar(context),
       body: child,
       bottomNavigationBar: bottomNavigationBar,
     );
   }
 
-  AppBar? renderAppBar() {
-    if (title == null) return null;
+  AppBar? renderAppBar(BuildContext context) {
+    if (title != null) {
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          title!,
+          style: const TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Pretendard',
+          ),
+        ),
+        foregroundColor: Colors.black,
+      );
+    }
+    if (titleWidget == null) {
+      return null;
+    }
     return AppBar(
       backgroundColor: Colors.white,
       elevation: 0,
-      title: Text(
-        title!,
-        style: const TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'Pretendard'),
+      titleSpacing: 0,
+      leading: IconButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        icon: Icon(
+          Icons.arrow_back_ios,
+          color: Colors.black,
+          size: 20.w,
+        ),
+      ),
+      title: Padding(
+        padding: EdgeInsets.only(right: 20.0.w),
+        child: titleWidget,
       ),
       foregroundColor: Colors.black,
     );
