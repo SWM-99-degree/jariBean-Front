8848594a8c62ea7a92b363d29d580723ec3688ee
lineno2 <hosun1804@gmail.com>
2023-08-19 11:11:50 +0900

    [FEAT] add truncate to quater hour function #46

--- lib/common/utils/utils.dart
+++ lib/common/utils/utils.dart
@@ -1,7 +1,18 @@
 import 'package:jari_bean/common/const/data.dart';
 
-class Utils{
-  static pathToUrl(String path){
+class Utils {
+  static pathToUrl(String path) {
     return '$ip/$path';
   }
+
+  static DateTime truncateToQuaterHour(DateTime date) {
+    return DateTime(
+      date.year,
+      date.month,
+      date.day,
+      date.hour,
+      (date.minute / 15).round() * 15,
+    );
+  }
+
 }
