b22a9b863b9802465ef1e4f3a882a78d4efa374e
lineno2 <hosun1804@gmail.com>
2023-08-19 11:20:43 +0900

    [FEAT] add search query provider #45

---
+++ lib/reservation/provider/search_query_provider.dart
@@ -0,0 +1,112 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:jari_bean/common/models/location_model.dart';
+import 'package:jari_bean/common/utils/utils.dart';
+import 'package:jari_bean/reservation/model/search_query_model.dart';
+
+final searchQueryProvider =
+    StateNotifierProvider<SearchQueryStateNotifier, SearchQueryModel>(
+  (ref) {
+    return SearchQueryStateNotifier();
+  },
+);
+
+class SearchQueryStateNotifier extends StateNotifier<SearchQueryModel> {
+  SearchQueryStateNotifier()
+      : super(
+          SearchQueryModel(
+            searchText: '',
+            serviceAreaId: null,
+            location: LocationModel(
+              latitude: 0,
+              longitude: 0,
+            ),
+            startTime: Utils.truncateToQuaterHour(
+              DateTime.now(),
+            ),
+            endTime: Utils.truncateToQuaterHour(
+              DateTime.now().add(
+                Duration(hours: 1),
+              ),
+            ),
+            headCount: 2,
+            tableOptionList: [],
+          ),
+        );
+
+  set serviceAreaId(String? serviceAreaId) {
+    state = state.copyWith(serviceAreaId: serviceAreaId);
+  }
+
+  set location(LocationModel location) {
+    state = state.copyWith(location: location);
+  }
+
+  set startTime(DateTime startTime) {
+    state = state.copyWith(startTime: startTime);
+  }
+
+  set endTime(DateTime endTime) {
+    state = state.copyWith(endTime: endTime);
+  }
+
+  set headCount(int headCount) {
+    state = state.copyWith(headCount: headCount);
+  }
+
+  set tableOptionList(List<TableType> tableOptionList) {
+    state = state.copyWith(tableOptionList: tableOptionList);
+  }
+
+  set dateFromDateTime(DateTime date) {
+    state = state.copyWith(
+      startTime: Utils.truncateToQuaterHour(
+        DateTime(
+          date.year,
+          date.month,
+          date.day,
+          state.startTime.hour,
+          state.startTime.minute,
+        ),
+      ),
+      endTime: Utils.truncateToQuaterHour(
+        DateTime(
+          date.year,
+          date.month,
+          date.day,
+          state.endTime.hour,
+          state.endTime.minute,
+        ),
+      ),
+    );
+  }
+
+  set startTimeFromDateTime(DateTime time) {
+    state = state.copyWith(
+      startTime: time,
+    );
+  }
+
+  set endTimeFromDateTime(DateTime time) {
+    state = state.copyWith(
+      endTime: time,
+    );
+  }
+
+  void toggleTableOptions(TableType tableType) {
+    final curtableOptionsList = state.tableOptionList;
+    if (isTableOptionsSelected(tableType)) {
+      state = state.copyWith(
+        tableOptionList: curtableOptionsList
+          ..removeWhere((element) => element == tableType),
+      );
+    } else {
+      state = state.copyWith(
+        tableOptionList: curtableOptionsList..add(tableType),
+      );
+    }
+  }
+
+  bool isTableOptionsSelected(TableType tableType) {
+    return state.tableOptionList.contains(tableType);
+  }
+}
