07f9c0ede9d65406cabb885a46295a9479631de0
lineno2 <hosun1804@gmail.com>
2023-08-20 21:13:18 +0900

    [REFACTOR] add  route and its screen #68

---
+++ lib/reservation/screen/result_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ResultScreen extends ConsumerWidget {
+  static String get routerName => '/result';
+  const ResultScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
