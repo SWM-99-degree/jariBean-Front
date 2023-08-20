4177f79adb22b3e85ba47f53ed69e02b62331499
lineno2 <hosun1804@gmail.com>
2023-08-20 14:35:46 +0900

    [REFACTOR] edit User Model and ViewModel to fit within server #64
    - user_model.dart : fit to server dto
    - user_repository.dart : edit path to `api/users`
    - user_provider.dart : erase former test codes and add temporary
      checking register logics

--- lib/user/models/user_model.g.dart
+++ lib/user/models/user_model.g.dart
@@ -7,22 +7,32 @@ part of 'user_model.dart';
 // **************************************************************************
 
 UserModel _$UserModelFromJson(Map<String, dynamic> json) => UserModel(
+      id: json['id'] as String,
       nickname: json['nickname'] as String,
-      socialLoginType:
-          $enumDecode(_$SocialLoginTypeEnumMap, json['socialLoginType']),
-      imgUrl: json['imgUrl'] as String,
-      isRegistered: json['isRegistered'] as bool?,
+      socialLoginType: $enumDecodeNullable(
+          _$SocialLoginTypeEnumMap, json['socialLoginType']),
+      imgUrl: json['imageUrl'] as String,
+      description: json['description'] as String?,
+      role: $enumDecode(_$RoleEnumMap, json['role']),
     );
 
 Map<String, dynamic> _$UserModelToJson(UserModel instance) => <String, dynamic>{
+      'id': instance.id,
       'nickname': instance.nickname,
-      'socialLoginType': _$SocialLoginTypeEnumMap[instance.socialLoginType]!,
-      'imgUrl': instance.imgUrl,
-      'isRegistered': instance.isRegistered,
+      'socialLoginType': _$SocialLoginTypeEnumMap[instance.socialLoginType],
+      'imageUrl': instance.imgUrl,
+      'description': instance.description,
+      'role': _$RoleEnumMap[instance.role]!,
     };
 
 const _$SocialLoginTypeEnumMap = {
   SocialLoginType.kakao: 'kakao',
   SocialLoginType.google: 'google',
   SocialLoginType.apple: 'apple',
+};
+
+const _$RoleEnumMap = {
+  Role.UNREGISTERED: 'UNREGISTERED',
+  Role.CUSTOMER: 'CUSTOMER',
+  Role.MANAGER: 'MANAGER',
 };
--- lib/user/repository/user_repository.g.dart
+++ lib/user/repository/user_repository.g.dart
@@ -19,33 +19,6 @@ class _UserRepository implements UserRepository {
   String? baseUrl;
 
   @override
-  Future<LoginResponseModel> login({
-    required type,
-    required body,
-  }) async {
-    const _extra = <String, dynamic>{};
-    final queryParameters = <String, dynamic>{};
-    final _headers = <String, dynamic>{};
-    final _data = <String, dynamic>{};
-    _data.addAll(body.toJson());
-    final _result = await _dio
-        .fetch<Map<String, dynamic>>(_setStreamType<LoginResponseModel>(Options(
-      method: 'POST',
-      headers: _headers,
-      extra: _extra,
-    )
-            .compose(
-              _dio.options,
-              '/login/${type}',
-              queryParameters: queryParameters,
-              data: _data,
-            )
-            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
-    final value = LoginResponseModel.fromJson(_result.data!);
-    return value;
-  }
-
-  @override
   Future<UserModel> getMe() async {
     const _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
@@ -78,7 +51,7 @@ class _UserRepository implements UserRepository {
     final Map<String, dynamic>? _data = null;
     final _result = await _dio
         .fetch<Map<String, dynamic>>(_setStreamType<UserModel>(Options(
-      method: 'POST',
+      method: 'PUT',
       headers: _headers,
       extra: _extra,
     )
@@ -90,6 +63,59 @@ class _UserRepository implements UserRepository {
             )
             .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
     final value = UserModel.fromJson(_result.data!);
+    return value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+}
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
+
+class _LoginRepository implements LoginRepository {
+  _LoginRepository(
+    this._dio, {
+    this.baseUrl,
+  });
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<LoginResponseModel> login({
+    required type,
+    required body,
+  }) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(body.toJson());
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<LoginResponseModel>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/${type}',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = LoginResponseModel.fromJson(_result.data!);
     return value;
   }
 
