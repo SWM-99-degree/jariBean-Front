5f8e50d16a8faf7a03dace24f11f02754b815bc4
lineno2 <hosun1804@gmail.com>
2023-09-04 19:22:03 +0900

    [FEAT] Add bottom sheet for each filter type #79

--- lib/common/component/custom_bottom_sheet.dart
+++ lib/common/component/custom_bottom_sheet.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jari_bean/reservation/screen/query_filter_screen.dart';
 
 class CustomBottomSheet extends ConsumerWidget {
   final Widget? child;
@@ -8,6 +9,47 @@ class CustomBottomSheet extends ConsumerWidget {
     this.child,
     super.key,
   });
+
+  factory CustomBottomSheet.fromFilterType(FilterType? filterType) {
+    final Widget? child;
+    switch (filterType) {
+      case FilterType.date:
+        child = CalendarFilter();
+      case FilterType.time:
+        child = Container(
+          padding: EdgeInsets.only(
+            left: 20.w,
+          ),
+          height: 120.h,
+          child: TimeFilter(),
+        );
+      case FilterType.headCount:
+        child = Container(
+          padding: EdgeInsets.only(
+            left: 20.w,
+          ),
+          height: 120.h,
+          child: HeadcountFilter(),
+        );
+      case FilterType.tableOption:
+        child = SizedBox(
+          height: 120.h,
+          child: TableOptionsFilter(),
+        );
+      default:
+        return CustomBottomSheet(
+          child: QueryFilterScreen(),
+        );
+    }
+    return CustomBottomSheet(
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          vertical: 20.h,
+        ),
+        child: child,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -35,3 +77,21 @@ showCustomBottomSheet({
   );
 }
 
+showCustomBottomSheetQueryFilter({
+  required BuildContext context,
+  FilterType? filterType,
+}) {
+  showModalBottomSheet(
+    context: context,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(30),
+        topRight: Radius.circular(30),
+      ),
+    ),
+    backgroundColor: Colors.white,
+    builder: (context) => CustomBottomSheet(
+      child: CustomBottomSheet.fromFilterType(filterType),
+    ),
+  );
+}
