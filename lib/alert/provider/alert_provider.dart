9a686c3f9c91407cd0c17bf6ca719819e2c02de6
lineno2 <hosun1804@gmail.com>
2023-09-11 23:38:53 +0900

    [FEAT] Add alertProvider #111

--- lib/alert/provider/alert_provider.dart
+++ lib/alert/provider/alert_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:jari_bean/alert/model/alert_model.dart';
+import 'package:jari_bean/common/models/fcm_message_model.dart';
 
 final alertProvider = StateNotifierProvider<AlertProvider, List<AlertModel>>(
   (ref) => AlertProvider(),
@@ -11,15 +12,34 @@ class AlertProvider extends StateNotifier<List<AlertModel>> {
           [
             AlertModel(
               id: 'test-id',
-              title: 'test',
-              description: 'this is a test',
-              buttonTitle: '테스트입니다',
-              image: 'https://picsum.photos/200/300',
+              title: '자리빈에 오신것을 환영합니다',
+              isRead: false,
+              body: '메뚜기 월드에 오신걸 환영합니다~',
+              type: PushMessageType.announcement,
+              receivedAt: DateTime.now(),
+              detailedBody: 'detailedBody',
+              data: FcmDataModelBase(),
             ),
           ],
         );
 
   AlertModel getAlertById(String id) {
     return state.firstWhere((element) => element.id == id);
+  }
+
+  void addAlertFromFcmMessage(FcmMessageModel message) {
+    if (state.any((element) => element.id == message.id)) {
+      return;
+    }
+    state = [
+      AlertModel.fromFcmMessage(message),
+      ...state,
+    ];
+  }
+
+  void markAsRead(String id) {
+    final alert = getAlertById(id);
+    alert.isRead = true;
+    state = [...state];
   }
 }
