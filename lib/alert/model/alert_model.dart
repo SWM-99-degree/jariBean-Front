d6ac55fe450947afb3d844f2385803f84d83328f
lineno2 <hosun1804@gmail.com>
2023-09-11 23:38:40 +0900

    [REFACTOR] Change AlertModel properties and make it extends FcmMessageModel #111

--- lib/alert/model/alert_model.dart
+++ lib/alert/model/alert_model.dart
@@ -1,18 +1,35 @@
-import 'package:jari_bean/common/models/model_with_id.dart';
+import 'package:jari_bean/common/models/fcm_message_model.dart';
 
-class AlertModel implements IModelWithId {
-  @override
-  String id;
-  final String title;
-  final String description;
-  final String buttonTitle;
-  final String image;
+class AlertModel extends FcmMessageModel {
+  final DateTime receivedAt;
+  final String? detailedBody;
+  bool isRead;
 
   AlertModel({
-    required this.id,
-    required this.title,
-    required this.description,
-    required this.buttonTitle,
-    required this.image,
-  });
+    required this.receivedAt,
+    required this.detailedBody,
+    required this.isRead,
+    required String id,
+    required String title,
+    required String body,
+    required PushMessageType type,
+    required FcmDataModelBase data,
+  }) : super(
+          id: id,
+          title: title,
+          body: body,
+          type: type,
+          data: data,
+        );
+
+  factory AlertModel.fromFcmMessage(FcmMessageModel message) => AlertModel(
+        receivedAt: DateTime.now(),
+        detailedBody: message.data.detailedBody,
+        isRead: false,
+        id: message.id,
+        title: message.title,
+        body: message.body,
+        type: message.type,
+        data: message.data,
+      );
 }
