82f1f40c3520f3f25ea843df55903a4ead32fadd
lineno2 <hosun1804@gmail.com>
2023-09-11 14:20:37 +0900

    [REFACTOR] Refactor homescreen and others. check #100

--- lib/common/screens/home_screen.dart
+++ lib/common/screens/home_screen.dart
@@ -1,109 +1,284 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:jari_bean/common/component/home_pointing_triangle.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jari_bean/common/const/color.dart';
-import 'package:jari_bean/common/provider/home_selection_provider.dart';
+import 'package:jari_bean/common/icons/jari_bean_icon_pack_icons.dart';
+import 'package:jari_bean/common/style/default_font_style.dart';
+import 'package:jari_bean/matching/provider/matching_timer_provider.dart';
 import 'package:jari_bean/matching/screen/matching_home_screen.dart';
+import 'package:jari_bean/matching/screen/matching_success_screen.dart';
 import 'package:jari_bean/reservation/screen/reservation_home_screen.dart';
 
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment(0.86, -0.52),
-          end: Alignment(-0.86, 0.52),
-          colors: const [PRIMARY_YELLOW, PRIMARY_ORANGE],
-        ),
-      ),
-      child: Column(
-        children: [
-          Flexible(
-            flex: 1,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                buildMenuBarItem(
-                  title: '자리 예약하기',
-                  updateFunction: () {
-                    ref
-                        .read(homeSelectionProvider.notifier)
-                        .update(HomeSelection.reservation);
-                  },
-                  homeSelectionState: ref.watch(homeSelectionProvider),
-                  homeSelectionValue: HomeSelection.reservation,
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  final ScrollController _scrollController =
+      ScrollController(initialScrollOffset: 50.h);
+  late final TabController _tabController;
+
+  bool flag = false;
+
+  final List<String> _tabs = ['자리 예약하기', '실시간 매칭하기'];
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this, initialIndex: 0);
+    _tabController.addListener(() {
+      setState(() {});
+      if (_tabController.indexIsChanging) {
+        _scrollController.animateTo(
+          ((flag && _tabController.index == 0) ? 176.h : 0) + 50.h,
+          duration: Duration(milliseconds: flag ? 250 : 150),
+          curve: Curves.easeInOut,
+        );
+      }
+    });
+    _scrollController.addListener(() {});
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    flag = ref.watch(matchingInfoProvider);
+    return DefaultTabController(
+      length: 2,
+      child: NestedScrollView(
+        controller: _scrollController,
+        headerSliverBuilder: (context, _) => [
+          SliverAppBar(
+            toolbarHeight: (flag) ? 176.h : 0,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.centerRight,
+                    end: Alignment.centerLeft,
+                    colors: const [PRIMARY_YELLOW, PRIMARY_ORANGE],
+                  ),
                 ),
-                buildMenuBarItem(
-                  title: '실시간 매칭하기',
-                  updateFunction: () {
-                    ref
-                        .read(homeSelectionProvider.notifier)
-                        .update(HomeSelection.matching);
-                  },
-                  homeSelectionState: ref.watch(homeSelectionProvider),
-                  homeSelectionValue: HomeSelection.matching,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    Icon(
+                      JariBeanIconPack.ok_sign,
+                      size: 52.w,
+                      color: Colors.white,
+                    ),
+                    SizedBox(
+                      height: 8.h,
+                    ),
+                    Text(
+                      '매칭 성공',
+                      textAlign: TextAlign.center,
+                      style: defaultFontStyleWhite.copyWith(
+                        fontSize: 22.sp,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
-          Flexible(
-            flex: 9,
-            child: Container(
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                ),
-                color: Colors.white,
               ),
-              child:
-                  ref.watch(homeSelectionProvider) == HomeSelection.reservation
-                      ? ReservationHomeScreen()
-                      : MatchingHomeScreen(),
+            ),
+            backgroundColor: Colors.transparent,
+          ),
+          SliverPersistentHeader(
+            pinned: true,
+            delegate: _StickyTabBarDelegate(
+              TabBar(
+                indicator:
+                    TriangleTabIndicator(color: Colors.white, radius: 10),
+                indicatorPadding: EdgeInsets.only(bottom: 20),
+                controller: _tabController,
+                tabs: _tabs
+                    .map(
+                      (String name) => Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: Tab(
+                              child: Text(
+                                name,
+                                style: defaultFontStyleWhite.copyWith(
+                                  color: Colors.white.withOpacity(
+                                    _tabController.index == _tabs.indexOf(name)
+                                        ? 1
+                                        : 0.5,
+                                  ),
+                                  fontWeight: FontWeight.w800,
+                                  fontSize: 18.sp,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 12.h + 20,
+                          ),
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
             ),
           ),
         ],
+        body: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+            color: Colors.white,
+          ),
+          child: Padding(
+            padding: EdgeInsets.only(top: 30.h),
+            child: TabBarView(
+              controller: _tabController,
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                ReservationHomeScreen(),
+                flag ? MatchingSuccessScreen() : MatchingHomeScreen()
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _StickyTabBarDelegate extends SliverPersistentHeaderDelegate {
+  const _StickyTabBarDelegate(this.tabBar);
+
+  final TabBar tabBar;
+
+  @override
+  double get minExtent => 55.h + 20;
+
+  @override
+  double get maxExtent => 55.h + 20;
+
+  double scrollAnimationValue(double shrinkOffset) {
+    double value = (shrinkOffset) / (maxExtent - 20);
+    return value > 1 ? 1 : value;
+  }
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    return ClipPath(
+      clipBehavior: Clip.hardEdge,
+      clipper: CustomAppBar(),
+      child: Container(
+        height: 55.h + 20,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.centerRight,
+            end: Alignment.centerLeft,
+            colors: const [PRIMARY_YELLOW, PRIMARY_ORANGE],
+          ),
+        ),
+        child: TabBar(
+          tabs: tabBar.tabs,
+          controller: tabBar.controller,
+          indicator:
+              TriangleTabIndicator(color: Colors.white, radius: 10.w * 0.5),
+          indicatorPadding:
+              EdgeInsets.only(bottom: 20 * scrollAnimationValue(shrinkOffset)),
+        ),
       ),
     );
   }
 
-  Widget buildMenuBarItem({
-    required String title,
-    required Function()? updateFunction,
-    required HomeSelection homeSelectionState,
-    required HomeSelection homeSelectionValue,
-  }) {
-    return Expanded(
-      child: Center(
-        child: Stack(
-          alignment: Alignment.center,
-          fit: StackFit.expand,
-          children: [
-            Center(
-              child: TextButton(
-                onPressed: updateFunction,
-                child: Text(
-                  title,
-                  style: TextStyle(
-                    color: Colors.white.withOpacity(
-                      homeSelectionState == homeSelectionValue ? 1 : 0.5,
-                    ),
-                    fontFamily: 'Pretendard',
-                    fontWeight: FontWeight.w800,
-                    fontSize: 18 * 1.2,
-                  ),
-                ),
-              ),
-            ),
-            HomePointingTriangle(
-              isActive: homeSelectionState == homeSelectionValue,
-            ),
-          ],
-        ),
-      ),
+  @override
+  bool shouldRebuild(_StickyTabBarDelegate oldDelegate) {
+    return tabBar != oldDelegate.tabBar;
+  }
+}
+
+class TriangleTabIndicator extends Decoration {
+  final BoxPainter _painter;
+
+  TriangleTabIndicator({required Color color, required double radius})
+      : _painter = DrawTriangle(color);
+
+  @override
+  BoxPainter createBoxPainter([void onChanged]) => _painter;
+}
+
+class DrawTriangle extends BoxPainter {
+  late final Paint _paint;
+
+  DrawTriangle(Color color) {
+    _paint = Paint()
+      ..color = color
+      ..style = PaintingStyle.fill;
+  }
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    final edgeLength = min(11.w, 11.h);
+    final Offset triangleOffset = offset +
+        Offset(
+          configuration.size!.width / 2,
+          configuration.size!.height - edgeLength,
+        );
+    var path = Path();
+
+    path.moveTo(triangleOffset.dx, triangleOffset.dy);
+    path.lineTo(triangleOffset.dx + edgeLength, triangleOffset.dy + edgeLength);
+    path.lineTo(triangleOffset.dx - edgeLength, triangleOffset.dy + edgeLength);
+
+    path.close();
+    canvas.drawPath(path, _paint);
+  }
+}
+
+class CustomAppBar extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+
+    path.lineTo(0, size.height);
+    path.arcToPoint(
+      Offset(20, size.height - 20),
+      radius: const Radius.circular(20),
     );
+
+    path.lineTo(
+      size.width - 20,
+      size.height - 20,
+    );
+
+    path.arcToPoint(
+      Offset(size.width, size.height),
+      radius: const Radius.circular(20),
+    );
+
+    path.lineTo(size.width, 0);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
   }
 }
--- lib/matching/screen/matching_success_screen.dart
+++ lib/matching/screen/matching_success_screen.dart
@@ -4,9 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jari_bean/cafe/model/cafe_description_model.dart';
 import 'package:jari_bean/common/component/custom_button.dart';
 import 'package:jari_bean/common/const/color.dart';
-import 'package:jari_bean/common/icons/jari_bean_icon_pack_icons.dart';
 import 'package:jari_bean/common/layout/default_card_layout.dart';
-import 'package:jari_bean/common/layout/default_screen_layout.dart';
 import 'package:jari_bean/common/models/custom_button_model.dart';
 import 'package:jari_bean/common/style/default_font_style.dart';
 import 'package:jari_bean/common/utils/utils.dart';
@@ -18,116 +16,76 @@ class MatchingSuccessScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final timeLeftInSeconds = ref.watch(matchingTimerProvider);
-    return DefaultLayout(
-      child: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment(0.86, -0.52),
-            end: Alignment(-0.86, 0.52),
-            colors: const [PRIMARY_YELLOW, PRIMARY_ORANGE],
-          ),
-        ),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 176.h,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  Icon(
-                    JariBeanIconPack.ok_sign,
-                    size: 52.w,
-                    color: Colors.white,
-                  ),
-                  SizedBox(
-                    height: 8.h,
-                  ),
-                  Text(
-                    '매칭 성공',
-                    textAlign: TextAlign.center,
-                    style: defaultFontStyleWhite.copyWith(
-                      fontSize: 22.sp,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                ],
-              ),
+    return ListView(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
             ),
-            Expanded(
-              child: Container(
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                  ),
-                  color: Colors.white,
+            color: Colors.white,
+          ),
+          padding: EdgeInsets.all(20.w),
+          child: Column(
+            children: [
+              DefaultCardLayout.fromModel(
+                model: CafeDescriptionModel.fromJson({
+                  'cafeId': '1',
+                  'cafeName': '스타벅스 고대점',
+                  'cafeAddress': '서울특별시 성북구 고려대로 24실 51',
+                  'cafeImageUrl': 'https://picsum.photos/250?image=9',
+                }),
+              ),
+              SizedBox(
+                height: 24.h,
+              ),
+              Text(
+                '체크인 타이머',
+                style: defaultFontStyleBlack.copyWith(
+                  fontSize: 18.sp,
+                  fontWeight: FontWeight.w700,
                 ),
-                padding: EdgeInsets.all(20.w),
-                child: Column(
-                  children: [
-                    DefaultCardLayout.fromModel(
-                      model: CafeDescriptionModel.fromJson({
-                        'cafeId': '1',
-                        'cafeName': '스타벅스 고대점',
-                        'cafeAddress': '서울특별시 성북구 고려대로 24실 51',
-                        'cafeImageUrl': 'https://picsum.photos/250?image=9',
-                      }),
-                    ),
-                    SizedBox(
-                      height: 24.h,
-                    ),
-                    Text(
-                      '체크인 타이머',
-                      style: defaultFontStyleBlack.copyWith(
-                        fontSize: 18.sp,
-                        fontWeight: FontWeight.w700,
+              ),
+              SizedBox(
+                height: 24.h,
+              ),
+              _percentageBuilder(timeLeftInSeconds),
+              SizedBox(
+                height: 32.h,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: CustomButton.fromModel(
+                      model: CustomButtonModel(
+                        title: '매칭취소',
+                        onPressed: () {},
+                        isDisabled: true,
                       ),
                     ),
-                    SizedBox(
-                      height: 24.h,
+                  ),
+                  SizedBox(
+                    width: 12.w,
+                  ),
+                  Expanded(
+                    child: CustomButton.fromModel(
+                      model: CustomButtonModel(
+                        title: '길찾기',
+                        onPressed: () {
+                          ref
+                              .read(matchingTimerProvider.notifier)
+                              .initTimer(initTimeLeft: 600);
+                        },
+                      ),
                     ),
-                    _percentageBuilder(timeLeftInSeconds),
-                    SizedBox(
-                      height: 32.h,
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: CustomButton.fromModel(
-                            model: CustomButtonModel(
-                              title: '매칭취소',
-                              onPressed: () {},
-                              isDisabled: true,
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          width: 12.w,
-                        ),
-                        Expanded(
-                          child: CustomButton.fromModel(
-                            model: CustomButtonModel(
-                              title: '길찾기',
-                              onPressed: () {
-                                ref
-                                    .read(matchingTimerProvider.notifier)
-                                    .initTimer(initTimeLeft: 600);
-                              },
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
+                  )
+                ],
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
+      ],
     );
   }
 
