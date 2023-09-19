75283998103cee2a8fa83dae51a2798b9f980f8f
lineno2 <hosun1804@gmail.com>
2023-09-19 23:59:29 +0900

    [FEAT] Add fcm token refresh logics #6

--- lib/common/firebase/fcm.dart
+++ lib/common/firebase/fcm.dart
@@ -1,19 +1,30 @@
 import 'dart:io';
+import 'package:dio/dio.dart' hide Headers;
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:jari_bean/alert/provider/alert_provider.dart';
+import 'package:jari_bean/common/const/data.dart';
+import 'package:jari_bean/common/dio/dio.dart';
 import 'package:jari_bean/common/models/fcm_message_model.dart';
 import 'package:jari_bean/common/notification/notification.dart';
 
 final fcmTokenProvider =
     StateNotifierProvider<FcmTokenStateNotifier, String>((ref) {
-  return FcmTokenStateNotifier();
+  final dio = ref.watch(dioProvider);
+  return FcmTokenStateNotifier(
+    dio,
+  );
 });
 
 class FcmTokenStateNotifier extends StateNotifier<String> {
-  FcmTokenStateNotifier() : super('') {
+  final Dio dio;
+  FcmTokenStateNotifier(
+    this.dio,
+  ) : super('') {
     getToken();
+    uploadToken();
   }
 
   Future<void> getToken() async {
@@ -22,11 +33,39 @@ class FcmTokenStateNotifier extends StateNotifier<String> {
     state = token!;
   }
 
+  Future<void> deleteToken() async {
+    final messaging = FirebaseMessaging.instance;
+    await messaging.deleteToken();
+    state = '';
+  }
 
+  Future<void> updateToken(String token) async {
+    state = token;
+    uploadToken();
+  }
+
+  Future<void> uploadToken() async {
+    final token = state;
+    await dio.put(
+      '$ip/api/fcm/token',
+      data: {
+        'firebaseToken': token,
+      },
+      options: Options(
+        headers: {
+          'accessToken': 'true',
+        },
+      ),
+    );
+  }
 }
 
-Future<void> fcmTokenRefreshHandler(String token) async {
+Future<void> fcmTokenRefreshHandler(
+  String token,
+  FcmTokenStateNotifier notifier,
+) async {
   print('Handling a token refresh $token');
+  notifier.updateToken(token);
 }
 
 @pragma('vm:entry-point')
@@ -34,14 +73,14 @@ Future<void> fcmMessageHandler(
   RemoteMessage message,
   ProviderContainer container,
 ) async {
+  await Firebase.initializeApp();
   print('Handling a message ${message.messageId}, ${message.data['title']}');
   final receivedNotification = FcmMessageModel.fromFcmMessage(message);
   container.read(alertProvider.notifier).addAlertFromFcmMessage(
         receivedNotification,
       );
   if (Platform.isIOS) return;
-  final notification =
-      container.read(notificationProvider.notifier);
+  final notification = container.read(notificationProvider.notifier);
   await notification.showFromFcmMessage(message: receivedNotification);
 }
 
