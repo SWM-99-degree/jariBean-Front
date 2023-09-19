cfb1a869c93ff86b9c2881b54bd0231fe6c094c1
lineno2 <hosun1804@gmail.com>
2023-09-19 23:27:25 +0900

    [REFACTOR] Extract useful function from FcmMessageModel to Utils #126

--- lib/common/utils/utils.dart
+++ lib/common/utils/utils.dart
@@ -1,4 +1,5 @@
 import 'package:jari_bean/common/const/data.dart';
+import 'package:jari_bean/common/models/fcm_message_model.dart';
 
 class Utils {
   static pathToUrl(String path) {
@@ -30,5 +31,70 @@ class Utils {
     int minutes = timeLeftInSeconds ~/ 60;
     int seconds = timeLeftInSeconds % 60;
     return "${minutes < 10 ? '0' : ''}$minutes : ${seconds < 10 ? '0' : ''}$seconds";
+  }
+
+  /// given type, return PushMessageType enum.
+  /// this is used to deserialize PushMessageType from json.
+  /// see this : https://github.com/SWM-99-degree/jariBean-Front/issues/126
+  static PushMessageType getPushMessageType(String type) {
+    switch (type) {
+      case 'matchingSuccess':
+        return PushMessageType.matchingSuccess;
+      case 'matchingFail':
+        return PushMessageType.matchingFail;
+      case 'matchingCancel':
+        return PushMessageType.matchingCancel;
+      case 'matchingUrgent':
+        return PushMessageType.matchingUrgent;
+      case 'announcement':
+        return PushMessageType.announcement;
+      case 'ads':
+        return PushMessageType.ads;
+      case 'reservationInfo':
+        return PushMessageType.reservationInfo;
+      case 'reservationComplete':
+        return PushMessageType.reservationComplete;
+      case 'reservationCanceled':
+        return PushMessageType.reservationCanceled;
+      case 'reservationUrgent':
+        return PushMessageType.reservationUrgent;
+      default:
+        throw Exception('Unknown push message type: $type');
+    }
+  }
+
+  /// given type and data, return FcmDataModelBase based on its type.
+  /// this is used to deserialize FcmDataModelBase from json.
+  /// for convenience, parameter `type` is String, not PushMessageType.
+  /// and it must be one of PushMessageType enum.
+  /// see this : https://github.com/SWM-99-degree/jariBean-Front/issues/126
+  static FcmDataModelBase getPushMessageData({
+    required String type,
+    required Map<String, dynamic> data,
+  }) {
+    switch (type) {
+      case 'matchingSuccess':
+        return MatchingSuccessModel.fromJson(data);
+      case 'matchingFail':
+        return MatchingFailModel.fromJson(data);
+      case 'matchingCancel':
+        return MatchingCancelModel.fromJson(data);
+      case 'matchingUrgent':
+        return MatchingUrgentModel.fromJson(data);
+      case 'announcement':
+        return AnnouncementDataModel.fromJson(data);
+      case 'ads':
+        return AdsDataModel.fromJson(data);
+      case 'reservationInfo':
+        return ReservationDataModel.fromJson(data);
+      case 'reservationComplete':
+        return ReservationDataModel.fromJson(data);
+      case 'reservationCanceled':
+        return ReservationDataModel.fromJson(data);
+      case 'reservationUrgent':
+        return ReservationDataModel.fromJson(data);
+      default:
+        throw Exception('Unknown push message type: ${data['type']}');
+    }
   }
 }
