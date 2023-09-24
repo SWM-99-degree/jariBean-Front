4ba28544ae336982947f1486ddb5e0a439b04d38
lineno2 <hosun1804@gmail.com>
2023-09-24 20:34:02 +0900

    [REFACTOR] Remove ShellRoute #134

--- lib/common/provider/go_router_provider.dart
+++ lib/common/provider/go_router_provider.dart
@@ -19,8 +19,6 @@ import 'package:jari_bean/user/screens/profile_screen.dart';
 import 'package:jari_bean/user/screens/register_screen.dart';
 
 final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>();
-final GlobalKey<NavigatorState> _shellNavigatorKey =
-    GlobalKey<NavigatorState>();
 
 final goRouterProvider = Provider<GoRouter>(
   (ref) {
@@ -33,32 +31,30 @@ final goRouterProvider = Provider<GoRouter>(
       redirect: provider.redirectAuthLogic,
       routes: [
         GoRoute(
-          path: '/splash',
-          name: SplashScreen.routerName,
-          builder: (_, __) => const SplashScreen(),
-        ),
-        GoRoute(
-          path: '/login',
-          name: LoginScreen.routerName,
-          builder: (_, __) => const LoginScreen(),
-        ),
-        GoRoute(
-          path: '/register',
-          name: RegisterScreen.routerName,
-          builder: (_, __) => const RegisterScreen(),
-          redirect: provider.redirectRegisterLogic,
-        ),
-        ShellRoute(
-          navigatorKey: _shellNavigatorKey,
-          builder: (context, state, child) {
-            print(child);
-            return RootScreen(
-              child: child,
-            );
-          },
+          path: '/',
+          name: RootScreen.routerName,
+          builder: (_, __) => const RootScreen(
+            child: HomeScreen(),
+          ),
           routes: [
             GoRoute(
-              path: '/home',
+              path: 'splash',
+              name: SplashScreen.routerName,
+              builder: (_, __) => const SplashScreen(),
+            ),
+            GoRoute(
+              path: 'login',
+              name: LoginScreen.routerName,
+              builder: (_, __) => const LoginScreen(),
+            ),
+            GoRoute(
+              path: 'register',
+              name: RegisterScreen.routerName,
+              builder: (_, __) => const RegisterScreen(),
+              redirect: provider.redirectRegisterLogic,
+            ),
+            GoRoute(
+              path: 'home',
               name: HomeScreen.routerName,
               pageBuilder: (context, state) {
                 if (state.queryParameters['selection'] == 'matching') {
@@ -72,69 +68,77 @@ final goRouterProvider = Provider<GoRouter>(
                       .update(HomeSelection.reservation);
                 }
                 return NoTransitionPage(
-                  child: HomeScreen(),
+                  child: RootScreen(
+                    child: HomeScreen(),
+                  ),
                 );
               },
             ),
             GoRoute(
-              path: '/history',
+              path: 'history',
               name: HistoryScreen.routerName,
               pageBuilder: (_, __) => NoTransitionPage(
-                child: HistoryScreen(),
+                child: RootScreen(
+                  child: HistoryScreen(),
+                ),
               ),
             ),
             GoRoute(
-              path: '/profile',
-              name: ProfileScreen.routerName,
-              pageBuilder: (_, __) => NoTransitionPage(
-                child: ProfileScreen(),
-              ),
-            ),
-            GoRoute(
-              path: '/alert',
+              path: 'alert',
               name: AlertScreen.routerName,
               pageBuilder: (_, __) => NoTransitionPage(
-                child: AlertScreen(),
+                child: RootScreen(
+                  child: AlertScreen(),
+                ),
               ),
             ),
-          ],
-        ),
-        GoRoute(
-          path: '/cafe',
-          name: CafeScreen.routerName,
-          builder: (_, state) => CafeScreen(),
-          routes: [
             GoRoute(
-              path: ":id",
-              builder: (_, state) =>
-                  CafeDetailScreen(cafeId: state.pathParameters['id']!),
+              path: 'profile',
+              name: ProfileScreen.routerName,
+              pageBuilder: (_, __) => NoTransitionPage(
+                child: RootScreen(
+                  child: ProfileScreen(),
+                ),
+              ),
+            ),
+            GoRoute(
+              path: 'cafe',
+              name: CafeScreen.routerName,
+              builder: (_, state) => CafeScreen(),
+              routes: [
+                GoRoute(
+                  path: ":id",
+                  builder: (_, state) =>
+                      CafeDetailScreen(cafeId: state.pathParameters['id']!),
+                ),
+              ],
+            ),
+            GoRoute(
+              path: 'search',
+              name: SearchScreen.routerName,
+              builder: (_, state) => SearchScreen(
+                serviceAreaId: state.queryParameters['serviceAreaId'],
+              ),
+            ),
+            GoRoute(
+              path: 'result',
+              name: ResultScreen.routerName,
+              builder: (_, __) => const ResultScreen(
+                cafeId: '1',
+              ),
+            ),
+            GoRoute(
+              path: 'matching/proceeding',
+              name: 'matching/proceeding',
+              builder: (_, __) => MatchingProceedingScreen(),
+            ),
+            GoRoute(
+              path: 'matching/success',
+              name: 'matching/success',
+              builder: (_, __) => MatchingSuccessScreen(),
             ),
           ],
         ),
-        GoRoute(
-          path: '/search',
-          name: SearchScreen.routerName,
-          builder: (_, state) => SearchScreen(
-            serviceAreaId: state.queryParameters['serviceAreaId'],
-          ),
-        ),
-        GoRoute(
-          path: '/result',
-          name: ResultScreen.routerName,
-          builder: (_, __) => const ResultScreen(
-            cafeId: '1',
-          ),
-        ),
-        GoRoute(
-          path: '/matching/proceeding',
-          name: '/matching/proceeding',
-          builder: (_, __) => MatchingProceedingScreen(),
-        ),
-        GoRoute(
-          path: '/matching/success',
-          name: '/matching/success',
-          builder: (_, __) => MatchingSuccessScreen(),
-        )
       ],
     );
   },
