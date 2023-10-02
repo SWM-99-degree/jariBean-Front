00d56a654e7afdb8a61b6d5487f4f4d144324750
lineno2 <hosun1804@gmail.com>
2023-10-02 12:29:04 +0900

    [FEAT] Add BookedDetail component #140

--- lib/common/layout/default_card_layout.dart
+++ lib/common/layout/default_card_layout.dart
@@ -5,17 +5,26 @@ import 'package:jari_bean/cafe/component/cafe_description_with_time.dart';
 import 'package:jari_bean/cafe/model/cafe_description_model.dart';
 import 'package:jari_bean/cafe/model/cafe_descripton_with_time_left_model.dart';
 import 'package:jari_bean/common/const/color.dart';
+import 'package:jari_bean/common/style/default_font_style.dart';
+import 'package:jari_bean/history/component/booked_details.dart';
+import 'package:jari_bean/history/model/booked_details_model.dart';
+import 'package:jari_bean/history/model/matching_model.dart';
+import 'package:jari_bean/history/model/reservation_model.dart';
 
 class DefaultCardLayout extends StatelessWidget {
   final String id;
   final String name;
   final String imgUrl;
   final Widget child;
+  final Color color;
+  final bool isShadowVisible;
   const DefaultCardLayout({
     required this.id,
     required this.name,
     required this.imgUrl,
     required this.child,
+    this.color = PRIMARY_ORANGE,
+    this.isShadowVisible = false,
     super.key,
   });
 
@@ -42,6 +51,38 @@ class DefaultCardLayout extends StatelessWidget {
     );
   }
 
+  factory DefaultCardLayout.fromReservationModel({
+    required ReservationModel model,
+  }) {
+    return DefaultCardLayout(
+      id: model.reservationId,
+      name: model.model.title,
+      imgUrl: model.model.imgUrl,
+      color: Colors.transparent,
+      isShadowVisible: true,
+      child: buildCafeInfo(
+        cafeModel: model.model,
+        bookedModel: BookedDetailsModel.fromReservationModel(model: model),
+      ),
+    );
+  }
+
+  factory DefaultCardLayout.fromMatchingModel({
+    required MatchingModel model,
+  }) {
+    return DefaultCardLayout(
+      id: model.matchingId,
+      name: model.model.title,
+      imgUrl: model.model.imgUrl,
+      color: Colors.transparent,
+      isShadowVisible: true,
+      child: buildCafeInfo(
+        cafeModel: model.model,
+        bookedModel: BookedDetailsModel.fromMatchingModel(model: model),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -52,9 +93,19 @@ class DefaultCardLayout extends StatelessWidget {
           width: 335.w,
           decoration: ShapeDecoration(
             shape: RoundedRectangleBorder(
-              side: BorderSide(width: 1.w, color: PRIMARY_ORANGE),
+              side: BorderSide(width: 1.w, color: color),
               borderRadius: BorderRadius.circular(8),
             ),
+            color: Colors.white,
+            shadows: [
+              if (isShadowVisible)
+                BoxShadow(
+                  color: Color(0x14000000),
+                  blurRadius: 10,
+                  offset: Offset(3, 3),
+                  spreadRadius: 0,
+                )
+            ],
           ),
           child: Row(
             children: [
@@ -77,4 +128,34 @@ class DefaultCardLayout extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget buildCafeInfo({
+  required CafeDescriptionModel cafeModel,
+  required BookedDetailsModel bookedModel,
+}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        cafeModel.title,
+        style: defaultFontStyleBlack.copyWith(
+          fontSize: 14.sp,
+          fontWeight: FontWeight.w700,
+          height: 1.5,
+        ),
+      ),
+      Text(
+        cafeModel.cafeAddress,
+        style: defaultFontStyleBlack.copyWith(
+          fontSize: 12.sp,
+          fontWeight: FontWeight.w500,
+          color: TEXT_SUBTITLE_COLOR,
+        ),
+      ),
+      BookedDetails.fromModel(
+        model: bookedModel,
+      )
+    ],
+  );
 }
--- lib/history/screens/history_screen.dart
+++ lib/history/screens/history_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:jari_bean/common/layout/default_card_layout.dart';
+import 'package:jari_bean/history/model/matching_model.dart';
+import 'package:jari_bean/history/model/reservation_model.dart';
 
 class HistoryScreen extends ConsumerWidget {
   static String get routerName => '/history';
@@ -7,6 +10,36 @@ class HistoryScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return Column(
+      children: [
+        DefaultCardLayout.fromMatchingModel(
+          model: MatchingModel.fromJson({
+            "id": "6503b645b723d27a6739687a",
+            "seating": 3,
+            "startTime": "2023-09-15T01:41:25.286",
+            "cafeSummaryDto": {
+              "id": "64fd48821a11b172e165f2fd",
+              "name": "스타벅스 테헤란로아남타워점",
+              "address": "아남타워빌딩 1층",
+              "imageUrl": "https://picsum.photos/50/50"
+            }
+          }),
+        ),
+        DefaultCardLayout.fromReservationModel(
+          model: ReservationModel.fromJson({
+            "reserveId": "6503b645b723d27a6739687a",
+            "reserveStartTime": "2023-09-15T01:41:25.286",
+            "reserveEndTime": "2023-09-15T11:41:25.286",
+            "matchingSeating": 3,
+            "cafeSummaryDto": {
+              "id": "64fd48821a11b172e165f2fd",
+              "name": "스타벅스 테헤란로아남타워점",
+              "address": "아남타워빌딩 1층",
+              "imageUrl": "https://picsum.photos/50/50"
+            }
+          }),
+        ),
+      ],
+    );
   }
 }
