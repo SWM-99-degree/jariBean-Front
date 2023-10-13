fa2d29b4fe10410a98f38493e7426626be6ffbc8
lineno2 <hosun1804@gmail.com>
2023-10-13 12:31:34 +0900

    [FEAT] Add PATCH:api/users in UserRepository #161

--- lib/user/repository/user_repository.g.dart
+++ lib/user/repository/user_repository.g.dart
@@ -74,6 +74,37 @@ class _UserRepository implements UserRepository {
     return value;
   }
 
+  @override
+  Future<dynamic> updateProfile({required FormData body}) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{
+      r'accessToken': 'true',
+      r'Content-Type': 'multipart/form-data',
+    };
+    _headers.removeWhere((k, v) => v == null);
+    final _data = body;
+    final _result = await _dio.fetch(_setStreamType<dynamic>(Options(
+      method: 'PATCH',
+      headers: _headers,
+      extra: _extra,
+      contentType: 'multipart/form-data',
+    )
+        .compose(
+          _dio.options,
+          '/',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        ))));
+    final value = _result.data;
+    return value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
