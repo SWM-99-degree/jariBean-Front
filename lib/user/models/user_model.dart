d2ea6294d4f0d565a1bace4690e1b6d95d330260
lineno2 <hosun1804@gmail.com>
2023-10-13 12:16:35 +0900

    [FEAT] Add UpdateProfileModel #161

--- lib/user/models/user_model.dart
+++ lib/user/models/user_model.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: constant_identifier_names
 
+import 'package:dio/dio.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:json_annotation/json_annotation.dart';
+import 'package:http_parser/http_parser.dart';
 
 part 'user_model.g.dart';
 
@@ -39,4 +42,50 @@ class UserModel extends UserModelBase {
 
   factory UserModel.fromJson(Map<String, dynamic> json) =>
       _$UserModelFromJson(json);
+}
+
+class UpdateProfileModel {
+  final String? nickname;
+  final String? description;
+  final FilePickerResult? imgFile;
+
+  UpdateProfileModel({
+    this.nickname,
+    this.description,
+    this.imgFile,
+  });
+
+  copyWith({
+    String? nickname,
+    String? description,
+    FilePickerResult? imgFile,
+  }) {
+    return UpdateProfileModel(
+      nickname: nickname ?? this.nickname,
+      description: description ?? this.description,
+      imgFile: imgFile ?? this.imgFile,
+    );
+  }
+
+  FormData toFormData() {
+    final formData = FormData();
+    if (nickname != null) {
+      formData.fields.add(MapEntry('username', nickname!));
+    }
+    if (description != null) {
+      formData.fields.add(MapEntry('description', description!));
+    }
+    if (imgFile != null) {
+      final fileName = imgFile!.files.first.name;
+      final String fileExtension = imgFile!.files.first
+          .extension!; // assume that selected file must have extension
+      final file = MultipartFile.fromBytes(
+        imgFile!.files.first.bytes!,
+        filename: fileName,
+        contentType: MediaType('image', fileExtension),
+      );
+      formData.files.add(MapEntry('image', file));
+    }
+    return formData;
+  }
 }
