161d49b58010736d1f7a50fb03cdc458319d3345
lineno2 <hosun1804@gmail.com>
2023-10-14 17:28:51 +0900

    [FIX] Fix pagiantion bug. check #165, #163

--- lib/reservation/provider/search_result_provider.dart
+++ lib/reservation/provider/search_result_provider.dart
@@ -1,41 +1,74 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:jari_bean/cafe/model/cafe_description_model.dart';
 import 'package:jari_bean/cafe/repository/cafe_repository.dart';
 import 'package:jari_bean/common/models/offset_pagination_model.dart';
+import 'package:jari_bean/common/models/pagination_params.dart';
+import 'package:jari_bean/common/provider/pagination_base_provider.dart';
 import 'package:jari_bean/reservation/model/search_query_model.dart';
 import 'package:jari_bean/reservation/provider/search_query_provider.dart';
 import 'package:jari_bean/reservation/provider/search_text_provider.dart';
 
-final searchResultProvider = StateNotifierProvider.autoDispose<
-    SearchResultStateNotifier, OffsetPaginationBase>(
+final searchResultProvider =
+    StateNotifierProvider<SearchResultStateNotifier, OffsetPaginationBase>(
   (ref) {
     final repository = ref.watch(cafeRepositoryProvider);
     final searchQuery = ref.watch(searchQueryProvider);
     final searchQueryText = ref.watch(searchTextProvider);
     return SearchResultStateNotifier(
-      repository: repository,
+      repository: Future((() => repository)),
       searchQuery: searchQuery,
       searchQueryText: searchQueryText,
     );
   },
 );
 
-class SearchResultStateNotifier extends StateNotifier<OffsetPaginationBase> {
-  final CafeRepository repository;
+class SearchResultStateNotifier extends PaginationBaseStateNotifier<
+    CafeDescriptionModel, CafeSearchResultRepository> {
   final SearchQueryModel searchQuery;
   final String searchQueryText;
   SearchResultStateNotifier({
-    required this.repository,
     required this.searchQuery,
     required this.searchQueryText,
-  }) : super(OffsetPaginationLoading());
+    required super.repository,
+  }) {
+    search();
+  }
 
-  Future<void> search() async {
+  /// searchQueryText is separated from searchQuery, so we need to merge them.
+  SearchQueryModel mergeSearchQuery({
+    required SearchQueryModel searchQuery,
+    required String searchQueryText,
+  }) {
+    return searchQuery.copyWith(
+      searchText: searchQueryText,
+    );
+  }
+
+  Future<void> search({
+    int fetchCount = 20,
+    int page = 0,
+    bool fetchMore = false,
+    bool forceRefetch = false,
+  }) async {
     try {
-      final searchQuery = this.searchQuery.copyWith(
-            searchText: searchQueryText,
-          );
-      final result = await repository.search(searchQuery);
-      state = result;
+      final query = mergeSearchQuery(
+        searchQuery: searchQuery,
+        searchQueryText: searchQueryText,
+      );
+      final paginateFunction = (await repository).paginateWithBody;
+      paginate(
+        fetchCount: fetchCount,
+        page: page,
+        fetchMore: fetchMore,
+        forceRefetch: forceRefetch,
+        paginate: ({
+          required PaginationParams paginationParams,
+        }) =>
+            paginateFunction(
+          paginationParams: paginationParams,
+          body: query,
+        ),
+      );
     } catch (e) {
       print(e);
       state = OffsetPaginationError(
