67a91b993f64c9e5994cfcfb7b888163656b0aea
lineno2 <hosun1804@gmail.com>
2023-10-16 16:39:19 +0900

    [REFACTOR] Edit provider structure. check #83

--- lib/cafe/provider/table_provider.dart
+++ lib/cafe/provider/table_provider.dart
@@ -1,58 +1,38 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:jari_bean/cafe/model/cafe_total_information_model.dart';
 import 'package:jari_bean/cafe/model/table_display_model.dart';
-import 'package:jari_bean/cafe/model/table_model.dart';
-import 'package:jari_bean/cafe/repository/table_repository.dart';
+import 'package:jari_bean/cafe/provider/cafe_provider.dart';
 import 'package:jari_bean/reservation/model/search_query_model.dart';
 import 'package:jari_bean/reservation/provider/search_query_provider.dart';
 
-final tableProvider = StateNotifierProvider.family<TableStateNotifier,
-    List<TableDetailModel>, String>((ref, id) {
-  final tableRepository = ref.watch(tableRepositoryProvider);
-  return TableStateNotifier(
-    cafeId: id,
-    repository: tableRepository,
-  );
-});
-
-class TableStateNotifier extends StateNotifier<List<TableDetailModel>> {
-  final String cafeId;
-  final TableRepository repository;
-  TableStateNotifier({
-    required this.cafeId,
-    required this.repository,
-  }) : super([]) {
-    getTables();
-  }
-
-  Future<void> getTables() async {
-    final tables = await repository.getTables(cafeId);
-    state = tables;
-  }
-}
-
-final tableDisplayProvider = StateNotifierProvider.family<
-    TableDisplayStateNotifier, List<TableDisplayModel>, String>(
+final tableDisplayProvider = StateNotifierProvider.family
+    .autoDispose<TableDisplayStateNotifier, List<TableDisplayModel>, String>(
   (ref, id) {
-    final tableModels = ref.watch(tableProvider(id));
+    final cafeModel = ref.watch(cafeInformationProvider(id));
     final searchQueryFilter = ref.watch(searchQueryProvider);
     return TableDisplayStateNotifier(
-      tableModels: tableModels,
+      cafeModel: cafeModel,
       queryFilter: searchQueryFilter,
     );
   },
 );
 
 class TableDisplayStateNotifier extends StateNotifier<List<TableDisplayModel>> {
-  final List<TableDetailModel> tableModels;
+  final CafeTotalInformationBase cafeModel;
   final SearchQueryModel queryFilter;
   TableDisplayStateNotifier({
-    required this.tableModels,
+    required this.cafeModel,
     required this.queryFilter,
   }) : super([]) {
     getTableDisplay();
   }
 
   Future<void> getTableDisplay() async {
+    if (cafeModel is! CafeTotalInformationModel) {
+      return; // cafeModel must be loaded because this provider is called after cafe data and its screen rendering is done.
+    }
+    final tableModels =
+        (cafeModel as CafeTotalInformationModel).tableDetailModelList;
     final tableDisplayList = tableModels.map((model) {
       return TableDisplayModel.calculateAvailablityFromTableModel(
         model: model,
