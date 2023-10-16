18165a4509a670597c8398687ea31aef45c7d3a4
lineno2 <hosun1804@gmail.com>
2023-10-16 16:39:19 +0900

    [REFACTOR] Edit provider structure. check #83

--- lib/cafe/screen/cafe_detail_table_screen.dart
+++ lib/cafe/screen/cafe_detail_table_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:jari_bean/reservation/screen/result_screen.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jari_bean/cafe/component/table_description.dart';
+import 'package:jari_bean/cafe/provider/table_provider.dart';
+import 'package:jari_bean/reservation/provider/search_query_provider.dart';
+import 'package:jari_bean/reservation/screen/simplified_filter_screen.dart';
 
 class CafeDetailTableScreen extends ConsumerWidget {
   final String cafeId;
@@ -11,6 +15,28 @@ class CafeDetailTableScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ResultScreen(cafeId: cafeId);
+    final tableDisplayList = ref.watch(tableDisplayProvider(cafeId));
+    final param = ref.watch(searchQueryProvider);
+    return Column(
+      children: [
+        SimplifiedFilterScreen(),
+        Expanded(
+          child: ListView.builder(
+            itemCount: tableDisplayList.length,
+            padding: EdgeInsets.only(
+              bottom: 10.h,
+            ),
+            itemBuilder: (context, index) {
+              final tableDisplay = tableDisplayList[index];
+              return TableDescriptionCard.fromModel(
+                model: tableDisplay,
+                startTime: param.startTime,
+                endTime: param.endTime,
+              );
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
