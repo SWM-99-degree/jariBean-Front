291483b1f663474eb76b455bb11e63df0a2ad675
lineno2 <hosun1804@gmail.com>
2023-10-20 12:55:06 +0900

    [REFACTOR] Make tableDisplayProvider use tableRepository #83

--- lib/reservation/provider/table_provider.dart
+++ lib/reservation/provider/table_provider.dart
@@ -1,53 +1,70 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:jari_bean/cafe/model/cafe_total_information_model.dart';
 import 'package:jari_bean/cafe/model/table_display_model.dart';
-import 'package:jari_bean/cafe/provider/cafe_provider.dart';
+import 'package:jari_bean/cafe/model/table_model.dart';
 import 'package:jari_bean/reservation/model/search_query_model.dart';
+import 'package:jari_bean/reservation/model/table_query_model.dart';
 import 'package:jari_bean/reservation/provider/search_query_provider.dart';
+import 'package:jari_bean/reservation/repository/table_repository.dart';
 
 final tableDisplayProvider = StateNotifierProvider.family
     .autoDispose<TableDisplayStateNotifier, List<TableDisplayModel>, String>(
   (ref, id) {
-    final cafeModel = ref.watch(cafeInformationProvider(id));
+    final repository = ref.watch(tableRepositoryProvider);
     final searchQueryFilter = ref.watch(searchQueryProvider);
     return TableDisplayStateNotifier(
-      cafeModel: cafeModel,
+      cafeId: id,
+      repository: repository,
       queryFilter: searchQueryFilter,
     );
   },
 );
 
 class TableDisplayStateNotifier extends StateNotifier<List<TableDisplayModel>> {
-  final CafeTotalInformationBase cafeModel;
+  final String cafeId;
+  final TableRepository repository;
   final SearchQueryModel queryFilter;
   TableDisplayStateNotifier({
-    required this.cafeModel,
+    required this.cafeId,
+    required this.repository,
     required this.queryFilter,
   }) : super([]) {
     getTableDisplay();
   }
 
-  Future<void> getTableDisplay() async {
-    if (cafeModel is! CafeTotalInformationModel) {
-      return; // cafeModel must be loaded because this provider is called after cafe data and its screen rendering is done.
-    }
-    final tableModels =
-        (cafeModel as CafeTotalInformationModel).tableDetailModelList;
-    final tableDisplayList = tableModels.map((model) {
+  Future<List<TableDetailModel>> getTables() async {
+    return await repository.getTables(
+      id: cafeId,
+      query: TableQueryModel(
+        startTime: queryFilter.startTime,
+        endTime: queryFilter.endTime,
+        headCount: queryFilter.headCount,
+        tableOptionList: queryFilter.tableOptionList,
+      ),
+    );
+  }
+
+  void setToTableDisplay(List<TableDetailModel> tableModels) {
+    state = tableModels.map((model) {
       return TableDisplayModel.calculateAvailablityFromTableModel(
         model: model,
         queryStartTime: queryFilter.startTime,
         queryEndTime: queryFilter.endTime,
       );
     }).toList();
-    state = tableDisplayList;
+  }
+
+  Future<void> getTableDisplay() async {
+    final tableModels = await getTables();
+    setToTableDisplay(tableModels);
     applyFilter();
   }
 
   void applyFilter() {
     final filteredList = state.where((element) {
       // if (!element.isAvaliable) return false;
-      if (element.tableModel.maxHeadcount < queryFilter.headCount) return false;
+      if (element.tableModel.maxHeadcount < queryFilter.headCount) {
+        return false;
+      }
       if (queryFilter.tableOptionList
           .every((e) => element.tableModel.tableOptionsList.contains(e))) {
         return true;
