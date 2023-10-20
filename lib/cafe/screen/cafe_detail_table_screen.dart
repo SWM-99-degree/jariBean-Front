a3a35e9806217e7b074d3c07b5cd734e0ea4278a
lineno2 <hosun1804@gmail.com>
2023-10-20 21:00:51 +0900

    [REFACTOR] Add tableProvider to change tableDisplayProvider logic #83

--- lib/cafe/screen/cafe_detail_table_screen.dart
+++ lib/cafe/screen/cafe_detail_table_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jari_bean/cafe/component/table_description.dart';
-import 'package:jari_bean/cafe/provider/table_provider.dart';
+import 'package:jari_bean/reservation/provider/table_provider.dart';
 import 'package:jari_bean/reservation/provider/search_query_provider.dart';
 import 'package:jari_bean/reservation/screen/simplified_filter_screen.dart';
 
