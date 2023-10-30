fec5d98b3ad710dcbfc0b012af2e96314d9845e8
lineno2 <hosun1804@gmail.com>
2023-10-30 10:35:54 +0900

    [REFACTOR] Change agreement detail viewing logic #16

--- lib/common/component/custom_checkbox_tile.dart
+++ lib/common/component/custom_checkbox_tile.dart
@@ -3,24 +3,24 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jari_bean/common/const/color.dart';
 import 'package:jari_bean/common/icons/jari_bean_icon_pack_icons.dart';
 import 'package:jari_bean/common/style/default_font_style.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CustomCheckboxTile extends StatelessWidget {
   final bool isChecked;
   final String title;
-  final String description;
+  final String url;
   final Function(bool?) onChanged;
   const CustomCheckboxTile({
     super.key,
     required this.isChecked,
     required this.title,
-    required this.description,
+    required this.url,
     required this.onChanged,
   });
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      crossAxisAlignment: CrossAxisAlignment.end,
       children: [
         SizedBox(
           width: 18.w,
@@ -34,26 +34,37 @@ class CustomCheckboxTile extends StatelessWidget {
         SizedBox(
           width: 8.w,
         ),
-        Text.rich(
-          TextSpan(
-            style: defaultFontStyleBlack.copyWith(
-              fontSize: 14.sp,
-              fontWeight: FontWeight.w500,
-            ),
-            children: [
-              TextSpan(
-                text: title,
+        TextButton(
+          style: TextButton.styleFrom(
+            padding: EdgeInsets.zero,
+            alignment: Alignment.center,
+          ),
+          onPressed: () async {
+            Uri url = Uri.parse(this.url);
+            if (!await launchUrl(url)) {
+              throw Exception('Could not launch $url');
+            }
+          },
+          child: Text.rich(
+            TextSpan(
+              style: defaultFontStyleBlack.copyWith(
+                fontSize: 14.sp,
+                fontWeight: FontWeight.w500,
+                height: 1.0,
               ),
-              WidgetSpan(
-                alignment: PlaceholderAlignment.middle,
-                child: Center(
+              children: [
+                TextSpan(
+                  text: title,
+                ),
+                WidgetSpan(
+                  alignment: PlaceholderAlignment.middle,
                   child: Icon(
                     JariBeanIconPack.arrow_right,
                     size: 14.sp,
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ],
