2585acc3399e8ecd25afcee764306ef755297ab8
lineno2 <hosun1804@gmail.com>
2023-11-02 15:08:23 +0900

    [REFACTOR] Add LocationModel #188

--- lib/reservation/model/service_area_model.dart
+++ lib/reservation/model/service_area_model.dart
@@ -1,3 +1,4 @@
+import 'package:jari_bean/common/models/location_model.dart';
 import 'package:jari_bean/common/models/model_with_id.dart';
 import 'package:jari_bean/common/utils/utils.dart';
 import 'package:json_annotation/json_annotation.dart';
@@ -11,11 +12,13 @@ class ServiceAreaModel implements IModelWithId {
   final String name;
   @JsonKey(fromJson: Utils.pathToUrl)
   final String imgUrl;
+  final LocationModel location;
 
   ServiceAreaModel({
     required this.id,
     required this.name,
     required this.imgUrl,
+    required this.location,
   });
 
   factory ServiceAreaModel.fromJson(Map<String, dynamic> json) =>
