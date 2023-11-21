4463306fd3548a4165636f4c8a8f4a3c01401a01
lineno2 <hosun1804@gmail.com>
2023-11-21 18:56:22 +0900

    [FEAT] Add matchingInfoModel and its provider #201

--- lib/matching/provider/matching_timer_provider.dart
+++ lib/matching/provider/matching_timer_provider.dart
@@ -7,10 +7,69 @@ final matchingTimerProvider = StateNotifierProvider<TimerStateNotifier, int>(
   },
 );
 
-final matchingInfoProvider = Provider((ref) {
-  if (ref.watch(matchingTimerProvider) == 0) {
-    return false;
-  } else {
-    return true;
+final matchingInfoProvider =
+    StateNotifierProvider<MatchingInfoStateNotifier, MatchingInfoModel?>(
+  (ref) {
+    return MatchingInfoStateNotifier(
+      matchingId: '',
+      cafeId: '',
+      startTime: DateTime.now(),
+    );
+  },
+);
+
+class MatchingInfoStateNotifier extends StateNotifier<MatchingInfoModel?> {
+  final String matchingId;
+  final String cafeId;
+  final DateTime startTime;
+  MatchingInfoStateNotifier({
+    required this.matchingId,
+    required this.cafeId,
+    required this.startTime,
+  }) : super(null) {
+    if (matchingId == '') {
+      state = null;
+    } else {
+      state = MatchingInfoModel(
+        matchingId: matchingId,
+        cafeId: cafeId,
+        startTime: startTime,
+      );
+    }
   }
-});
+
+  void applyMatchingInfo({
+    required String matchingId,
+    required String cafeId,
+    required DateTime startTime,
+  }) {
+    state = MatchingInfoModel(
+      matchingId: matchingId,
+      cafeId: cafeId,
+      startTime: startTime,
+    );
+  }
+}
+
+class MatchingInfoModel {
+  final String matchingId;
+  final String cafeId;
+  final DateTime startTime;
+  MatchingInfoModel({
+    required this.matchingId,
+    required this.cafeId,
+    required this.startTime,
+  });
+
+  MatchingInfoModel copyWith({
+    String? matchingId,
+    String? cafeId,
+    DateTime? startTime,
+  }) {
+    return MatchingInfoModel(
+      matchingId: matchingId ?? this.matchingId,
+      cafeId: cafeId ?? this.cafeId,
+      startTime: startTime ?? this.startTime,
+    );
+  }
+}
